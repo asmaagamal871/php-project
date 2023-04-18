dd2c2422562a951aebca5e94178c2c805146db6c
asmaa <asmaagamal99192@gmail.com>
2023-04-19 01:42:31 +0200

    adminLTE structure

---
+++ views/partials/footer.php
@@ -0,0 +1,12 @@
+<footer>
+                <div class="pull-right">
+                    Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+                </div>
+                <div class="clearfix"></div>
+            </footer>
+            <!-- /footer content -->
+        </div>
+    </div>
+
+
+ 
