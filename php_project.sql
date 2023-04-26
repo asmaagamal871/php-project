869f19dac96a90da61c449bb9f578f572182b3e3
Shehab Hossam <Shehab.haz@gmail.com>
2023-04-26 21:24:59 +0200

    edited sql

--- php_project.sql
+++ php_project.sql
@@ -134,3 +134,7 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+/* Inserts*/
+-- INSERT INTO `groups` (`name`, `description`, `role`) VALUES ('Admins', 'This group is for admins', 'admin');
+-- INSERT INTO `groups` (`name`, `description`, `role`) VALUES ('Editors', 'This group is for editors', 'editor');
