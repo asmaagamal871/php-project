ecf17a5f58bda92b61e71e41a222f231d05fa837
asmaa <asmaagamal99192@gmail.com>
2023-04-26 03:20:44 +0200

    add favicon to login

--- app/views/login.php
+++ app/views/login.php
@@ -24,6 +24,8 @@
 
     <!-- Custom Theme Style -->
     <link href="/css/custom.css" rel="stylesheet">
+
+    <link rel="icon" type="image/x-icon" href="images/ball.ico">
   </head>
 
   <body class="login">
