7b9a573bcbfed5cbbd921a78a4cd4296a8f67e6e
Sondos11 <sondossaid40@gmail.com>
2023-04-28 23:10:12 +0300

    forms user validation

--- app/views/users/create.php
+++ app/views/users/create.php
@@ -19,27 +19,29 @@ include __DIR__ . '/../partials/header.php';
                     ?>
                     <label for="exampleInputPassword1" class="form-label mt-2 fw-bold">Name</label>
                     <div class="input-group mb-3">
-                        <input type="text" name="name" class="form-control" id="exampleInputUser1" required>
+                        <input type="text" maxlength="20" name="name" class="form-control" id="exampleInputUser1" required>
+                        <div class="invalid-feedback">Please enter your name.</div>
                     </div>
 
                     <label for="exampleInputPassword2" class="form-label mt-2 fw-bold">Email</label>
                     <div class="input-group mb-3">
-                        <input type="email" name="email" class="form-control" id="exampleInputUser1" required>
+                        <input type="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" id="email" name="email" class="form-control" required>
                     </div>
 
                     <label for="exampleInputPassword2" class="form-label mt-2 fw-bold">Phone</label>
                     <div class="input-group mb-3">
-                        <input type="text" name="phone" class="form-control" id="exampleInputUser1" required>
+                        <input type="tel" pattern="[0-9]{7,}" name="phone" id="phone" class="form-control" required>
                     </div>
-
                     <label for="exampleInputPassword2" class="form-label mt-2 fw-bold">Username</label>
                     <div class="input-group mb-3">
-                        <input type="text" name="username" class="form-control" id="exampleInputUser1" required>
+                        <input type="text" maxlength="10" name="username" class="form-control" id="exampleInputUser1" required>
+                        <div class="invalid-feedback">Please enter a username.</div>
                     </div>
 
                     <label for="exampleInputPassword2" class="form-label mt-2 fw-bold">Password</label>
                     <div class="input-group mb-3">
-                        <input type="password" name="password" class="form-control" id="exampleInputUser1" required>
+                        <input type="password" autocomplete="off" name="password" class="form-control" id="exampleInputUser1" required>
+                        <div class="invalid-feedback">Please enter a password.</div>
                     </div>
 
                     <label for="exampleInputPassword2" class="form-label mt-2 fw-bold">Group</label>
@@ -53,13 +55,15 @@ include __DIR__ . '/../partials/header.php';
                             }
                             ?>
                         </select><br>
+                        <div class="invalid-feedback">Please select a group.</div>
                     </div>
-
-                    <button type="submit" class="offset-4 px-4 btn btn-outline-dark">Create</button>
                 </div>
-            </form>
+
+                <button type="submit" class="offset-4 px-4 btn btn-outline-dark">Create</button>
         </div>
+        </form>
     </div>
+</div>
 
 </div>
 <!-- /page content -->
@@ -75,59 +79,29 @@ include __DIR__ . '/../partials/footer.php';
 <script src="../../vendors/validator/multifield.js"></script>
 <script src="../../vendors/validator/validator.js"></script>
 
-<!-- Javascript functions	-->
+<?php
+include __DIR__ . '/../partials/footer.php';
+?>
+<!-- validation -->
 <script>
-    function hideshow() {
-        var password = document.getElementById("password1");
-        var slash = document.getElementById("slash");
-        var eye = document.getElementById("eye");
-
-        if (password.type === 'password') {
-            password.type = "text";
-            slash.style.display = "block";
-            eye.style.display = "none";
+    const emailInput = document.getElementById("email");
+    const phoneInput = document.getElementById("phone");
+    emailInput.addEventListener("input", function() {
+        if (emailInput.validity.patternMismatch) {
+            emailInput.setCustomValidity("Please enter a valid email address.");
         } else {
-            password.type = "password";
-            slash.style.display = "none";
-            eye.style.display = "block";
+            emailInput.setCustomValidity("");
         }
-    }
+    });
+    phoneInpu.addEventListener("input", function() {
+        if (phoneInput.validity.patternMismatch) {
+            phoneInput.setCustomValidity("Please enter a valid phone number.");
+        } else {
+            phoneInput.setCustomValidity("");
+        }
+    });
 </script>
 
-<script>
-    // initialize a validator instance from the "FormValidator" constructor.
-    // A "<form>" element is optionally passed as an argument, but is not a must
-    var validator = new FormValidator({
-        "events": ['blur', 'input', 'change']
-    }, document.forms[0]);
-    // on form "submit" event
-    document.forms[0].onsubmit = function(e) {
-        var submit = true,
-            validatorResult = validator.checkAll(this);
-        console.log(validatorResult);
-        return !!validatorResult.valid;
-    };
-    // on form "reset" event
-    document.forms[0].onreset = function(e) {
-        validator.reset();
-    };
-    // stuff related ONLY for this demo page:
-    $('.toggleValidationTooltips').change(function() {
-        validator.settings.alerts = !this.checked;
-        if (this.checked)
-            $('form .alert').remove();
-    }).prop('checked', false);
-</script>
-
-<script>
-    var alert = document.getElementById("myAlert");
-
-    alert.style.display = "block";
-
-    setTimeout(function() {
-        alert.style.display = "none";
-    }, 1500);
-</script>
 <?php
 include __DIR__ . '/../partials/scripts.php';
 ?>
