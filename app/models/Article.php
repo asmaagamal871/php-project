bbafd38652ece18705bb11e9eecf43413285b179
nehad osman <nehad.osman19@gmail.com>
2023-04-28 16:26:28 +0300

    show articles of user how logged in only

--- app/models/Article.php
+++ app/models/Article.php
@@ -14,6 +14,7 @@ class Article
         return $this->db->get_all_records_paginated();
     }
 
+
     public function create()
     {
         return $this->db->save($_POST);
@@ -24,6 +25,10 @@ class Article
         return $this->db->get_record_by_id($id);
     }
 
+    public function getUserArticles($id)
+    {
+        return $this->db->get_record_by_id($id, 'user_id');
+    }
     public function restore($id)
     {
         return $this->db->restore($id);
