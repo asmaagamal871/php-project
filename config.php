7e80f68ee2ee5c4dc254e9a270c0db9fe70f68df
Shehab Hossam <Shehab.haz@gmail.com>
2023-04-29 13:58:53 +0300

    Clear Git cache

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("__HOST__", "127.0.0.1");
-define("__USER__", "root");
-define("__PASS__", "");
-define("__DB__", "php_project");
-define("__PORT__", "3306");
-define("__RECORDS_PER_PAGE__", 10);
-define("__Debug__Mode__", 0);
-define("__LOG__FILE__", "error.log");
