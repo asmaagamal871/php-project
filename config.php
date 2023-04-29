81f8b8f350dcbab8f1d71917844454e768e24261
asmaagamal871 <71777888+asmaagamal871@users.noreply.github.com>
2023-04-29 02:29:18 +0200

    delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("__HOST__", "127.0.0.1");
-define("__USER__", "root");
-define("__PASS__", "");
-define("__DB__", "php-project");
-define("__PORT__", "3325");
-define("__RECORDS_PER_PAGE__", 10);
-define("__Debug__Mode__", 0);
